24e032a59caef8cd35a709ab5136c0136a16576f
Petter Iversen <petter.iversen90@gmail.com>
2020-09-17 12:36:04 +0200

    test upload

---
+++ script.js
@@ -0,0 +1,49 @@
+//  question 1; Declare a variable called firstVar but don't initialise it with a value.
+
+// my answer for question 1
+var firstVar;
+
+//  question 2; Declare a variable called name and assign it your first name.
+
+// my answer for question 2
+var name = Petter;
+
+//  question 3; Declare and initialise a variable with a number value.
+
+// my answer for question 3
+var number = 7;
+
+//  question 4; Create a variable called division and initialise it with a value of 20 divided by 5.
+
+// my answer for question 4
+var division = 20 / 5;
+
+//  question 5; Write code that checks the type of the value "frog".
+
+// my answer for question 5
+typeof("frog");
+
+//  question 6; Declare and initialise a variable called orderHasShipped with a boolean value.
+
+// my answer for question 6
+var orderHasShipped = true;
+
+//  question 7; Create an if statement that checks if orderHasShipped is true.
+// If it is true, console log the string value "true". If not, console log the string value "false".
+
+// my answer for question 7
+if (orderHasShipped === true) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+//  question 8; Create a for loop that counts from 0 to 9. 
+// Console log the value of the counter variable inside the loop.
+
+// my answer for question 8
+for(var count = 0; count <= 9; count++) {
+    console.log(count);
+}
+
+// petter iversen
